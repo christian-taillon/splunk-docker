7a8c379291a489e710ee92839eda0b8c5bfbbac2
christian.taillon <christianjtaillon@gmail.com>
2021-02-20 21:12:58 -0700

    working

--- run-docker.sh
+++ run-docker.sh
@@ -2,5 +2,9 @@
 docker volume create so1-var
 # create volume for apps/config
 docker volume create so1-etc
+# create volume for user
+docker volume create so1-usr
+# create volume for system/local
+docker volume create so1-sysloc
 # run docker compose
-docker-compose up
+docker-compose up -d
